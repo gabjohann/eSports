051f45ddb7aa159cbb82726bb53455d4b0c339d6
Lucas Gabriel <gabif.johann@gmail.com>
2022-09-15 17:13:20 -0300

    aula 02 - mobile

---
+++ mobile/src/components/Background/index.tsx
@@ -0,0 +1,20 @@
+import { ImageBackground } from 'react-native';
+
+import backgroundImg from '../../assets/background-galaxy.png';
+
+import { styles } from './styles';
+
+interface Props {
+  children: React.ReactNode;
+}
+export function Background({ children }: Props) {
+  return (
+    <ImageBackground 
+      source={backgroundImg}
+      defaultSource={backgroundImg} // memoriza a imagem padrão, acelerando o processamento
+      style={styles.container}
+    >
+      {children}
+    </ImageBackground>
+  );
+}
